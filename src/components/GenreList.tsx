07d67c0fad1c14cd0a251d301abe05b7a681f165
Khashayar Darounparvar <khashayardaronparvar@gmail.com>
2025-04-12 18:48:08 -0400

    fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import useGenre from "../hooks/useGenre";
+
+const GenreList = () => {
+  const { genres } = useGenre();
+  return (
+    <div>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GenreList;
